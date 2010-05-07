af8541fb1719f201d4f971aa78eb2e708fe4a2a4
plfiorini <plfiorini@10171b4d-537d-4361-a55b-26d635fbb5c3>
2010-05-07 16:54:04 +0000

    Added an utility class for lists.

---
+++ libs/libsupport/List.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2009-2010, Pier Luigi Fiorini. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _LIST_H
+#define _LIST_H
+
+#include <list>
+
+#include <SupportDefs.h>
+
+template<class T>
+class List {
+public:
+	uint32 	CountItems() const;
+
+	void  	AddItem(T type);
+
+	void	RemoveItemAt(uint32 position);
+
+	TYPE	ItemAt(uint32 position);
+
+private:
+	std::list<T> fList;
+	typedef typename std::list<T>::iterator fIter;
+};
+
+
+template<class T>
+uint32 List<T>::CountItems() const
+{
+	return fList.size();
+}
+
+
+template<class T>
+void List<T>::AddItem(T type)
+{
+	fList.push(type);
+}
+
+
+template<class T>
+void List<T>::RemoveItemAt(uint32 position)
+{
+	fIter i = fMap.begin();
+	std::advance(i, position);
+	fList.erase(i);
+}
+
+
+template<class T>
+T List<T>::ItemAt(uint32 position)
+{
+	fIter i = fList.begin();
+	std::advance(i, position); 	
+	if (i == fList.end())
+		return NULL;
+	return i;
+}
+
+#endif	// _LIST_H
